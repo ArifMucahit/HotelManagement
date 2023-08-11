efe57069b5589777619d9bc858853993c2c1d34f
ArifMucahit <arifmf@hotmail.com>
2023-08-11 09:08:31 +0300

    middleware added

---
+++ ReportManagementAPI/Middleware/ExceptionHandler.cs
@@ -0,0 +1,49 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Diagnostics;
+using ReportManagementAPI.Models;
+
+namespace ReportManagementAPI.Middleware;
+
+public static class ExceptionHandler
+{
+    public static void UseCustomExceptionHandler(this IApplicationBuilder app)
+    {
+        app.UseExceptionHandler(err =>
+        {
+            err.Run(async context =>
+            {
+                var errorFeature = context.Features.Get<IExceptionHandlerFeature>();
+                var exception = errorFeature.Error.GetBaseException();
+
+                context.Response.Clear();
+                context.Response.ContentType = "application/json";
+
+                if (exception is ValidationException validationEx)
+                {
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    await context.Response.WriteAsJsonAsync(new ApiErrorResponse(validationEx.Message));
+                    return;
+                }
+
+                if (exception is BadHttpRequestException)
+                {
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    await context.Response.WriteAsJsonAsync(new ApiErrorResponse(exception.Message));
+                    return;
+                }
+
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                var errorMsg = exception.Message;
+
+                var env = context.RequestServices.GetService<IWebHostEnvironment>();
+                if (env.IsDevelopment())
+                {
+                    errorMsg += $"\nStackTrace: {exception.StackTrace}";
+                }
+
+                await context.Response.WriteAsJsonAsync(new ApiErrorResponse(errorMsg));
+            });
+        });
+    }
+}
